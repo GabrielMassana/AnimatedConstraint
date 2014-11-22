bded6c37872e0ae167543bd51c14de21d1852dc7
Your Name HereGabrielMassana <gabrielmassana@gmail.com>
2014-11-22 16:37:09 +0000

    Initial Commit

---
+++ animateConstraint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animateConstraint
+//
+//  Created by Jose Antonio Gabriel Massana on 22/11/14.
+//  Copyright (c) 2014 GabrielMassana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ animateConstraint/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  animateConstraint
+//
+//  Created by Jose Antonio Gabriel Massana on 22/11/14.
+//  Copyright (c) 2014 GabrielMassana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
